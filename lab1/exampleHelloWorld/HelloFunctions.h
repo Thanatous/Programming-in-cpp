0b34ed561009a7e5b644d1172681dab572d59ca8
Thanatous <36291648+Thanatous@users.noreply.github.com>
2018-03-23 01:32:56 +0300

    Create HelloFunctions.h

---
+++ lab1/exampleHelloWorld/HelloFunctions.h
@@ -0,0 +1,10 @@
+#ifndef SQUARE_V_HELLOFUNCTIONS_H
+#define SQUARE_V_HELLOFUNCTIONS_H
+
+#include <string>
+using namespace std;
+
+string GetHello();
+string GetAdressat(string adressat);
+
+#endif //SQUARE_V_HELLOFUNCTIONS_H
