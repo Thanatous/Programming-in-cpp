0535ed331885379d40d87869d3ffcaacd15e5235
Thanatous <36291648+Thanatous@users.noreply.github.com>
2018-03-22 21:51:36 +0300

    Create Variables.h

---
+++ lab1/SetUpGoogleTest/tests/Variables.h
@@ -0,0 +1,14 @@
+#ifndef LAB1_SQUARE_VOLUME_VARIABLES_H
+#define LAB1_SQUARE_VOLUME_VARIABLES_H
+
+double Rc1 = 25.34,
+        Hc1 = 12,
+        Rc2 = 3.56,
+        Hc2 = 10.54;
+
+double Rt1 = 20,
+        rt1 = 12.7,
+        Rt2 = 23.9,
+        rt2 = 0.8;
+
+#endif //LAB1_SQUARE_VOLUME_VARIABLES_H
