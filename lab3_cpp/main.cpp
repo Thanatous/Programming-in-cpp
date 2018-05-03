cddc5167591c8db35da9fa69c069c48af9756fea
Thanatous <36291648+Thanatous@users.noreply.github.com>
2018-05-03 18:21:29 +0300

    Update main.cpp

--- lab3_cpp/main.cpp
+++ lab3_cpp/main.cpp
@@ -3,6 +3,8 @@
 int main(){
     string M_1 = "/Users/Jane/CLionProjects/lab3_matrix/M_1.txt";
     string M_2 = "/Users/Jane/CLionProjects/lab3_matrix/M_2.txt";
+    string M_3 = "/Users/Jane/CLionProjects/lab3_matrix/M_3.txt";
+    string M_4 = "/Users/Jane/CLionProjects/lab3_matrix/M_4.txt";
 
     Matrix<int> matrix1;
     Matrix<int> matrix2;
@@ -17,13 +19,21 @@ int main(){
     matrix2.print(M_2);
     matrix2.show();
 
-    cout << "\nMatrix M_1 + Matrix M_2:" << endl;
-    matrix3 = matrix1 + matrix2;
+    cout << "\nMatrix M_3: " << endl;
+    matrix3.print(M_3);
     matrix3.show();
 
-    cout << "\nMatrix M_1 * Matrix M_2:" << endl;
-    matrix4 = matrix1 * matrix2;
+    cout << "\nMatrix M_4: " << endl;
+    matrix4.print(M_4);
     matrix4.show();
+
+    cout << "\nMatrix M_1 + Matrix M_2:" << endl;
+    (matrix1+=matrix2).show();
+
+    cout << "\nMatrix M_3 * Matrix M_4:" << endl;
+    (matrix3*matrix4).show();
 
     return 0;
 }
+
+
